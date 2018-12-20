91e1dda8ee245cc32c00c212aa23dfa5a1f00f4c
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-12-20 07:04:51 +0100

    fix: container version

--- src/addon.class.js
+++ src/addon.class.js
@@ -277,7 +277,7 @@ class Addon extends SafeEmitter {
             uid: this.uid,
             name: this.name,
             version: this.version,
-            containerVersion: "0.15.1",
+            containerVersion: "0.15.2",
             ready: this.isReady,
             started: this.isStarted,
             awake: this.isAwake,
